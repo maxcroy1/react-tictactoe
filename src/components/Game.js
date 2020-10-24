96c97a49220190d4556005f6883e2d4562c2c3e9
maxcroy1 <max.croy1@gmail.com>
2020-10-23 22:02:58 -0400

    last update

---
+++ src/components/Game.js
@@ -0,0 +1,13 @@
+export function calculateWinner(squares) {
+    const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,5],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ];
+    for(let )
+}
